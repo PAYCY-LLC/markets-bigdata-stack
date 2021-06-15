84c41e66e8d93f975b9526c8ce3430727168ae73
nk-the-crazy <nurkali@gmail.com>
2021-06-15 11:03:57 +0600

    Add custom Dockerfile for airflow and fix etl s3 task

--- docker-airflow-etl/dags/etl.py
+++ docker-airflow-etl/dags/etl.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 from airflow.operators.docker_operator import DockerOperator
+from airflow.operators.python_operator import PythonOperator
 
 import boto3
 import os
@@ -13,6 +14,11 @@ ENV_S3_ENDPOINT = os.getenv("S3_ENDPOINT")
 ENV_S3_REGION = os.getenv("S3_REGION")
 ENV_NODE_ETHEREUM = os.getenv("NODE_ETHEREUM")
 ENV_DATADIR_EXTERNAL = os.getenv("DATADIR_EXTERNAL")
+
+s3_conn_id = 's3-conn'
+state = 'wa'
+date = '{{ yesterday_ds_nodash }}'
+
 
 default_args = {
     'owner': 'airflow',
@@ -29,12 +35,13 @@ with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *",
 
     def upload_file(source, target):
 
+
         session = boto3.session.Session()
         client = session.client('s3',
                                 region_name=ENV_S3_REGION,
                                 endpoint_url=ENV_S3_ENDPOINT,
                                 aws_access_key_id=ENV_S3_ACCESS_KEY,
-                                aws_secret_access_key=ENV_S3_ACCESS_KEY)
+                                aws_secret_access_key=ENV_S3_SECRET_KEY)
 
         client.upload_file(source,  # Path to local file
                            ENV_S3_BUCKET_NAME,  # Name of Space
@@ -50,8 +57,8 @@ with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *",
         image='blockchainetl/ethereum-etl:latest',
         api_version='auto',
         auto_remove=True,
-        volumes = ['%s/etl:/ethereum-etl/output'.format(ENV_DATADIR_EXTERNAL), "/var/run/docker.sock:/var/run/docker.sock"],
-        command = "/bin/sleep 25",
+        volumes=[f'{ENV_DATADIR_EXTERNAL}/etl:/ethereum-etl/output'],
+        command=f'export_blocks_and_transactions -s 0 -e 100 --blocks-output /ethereum-etl/output/blocks.csv --transactions-output /ethereum-etl/output/transactions.csv -p {ENV_NODE_ETHEREUM}',
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge"
     )
@@ -60,7 +67,13 @@ with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *",
         task_id='print_hello',
         bash_command='echo "hello world"'
     )
-    t1 >> t2 >> t3
 
+    upload_to_s3 = PythonOperator(
+        task_id='upload_to_s3',
+        python_callable=upload_file,
+        op_kwargs={'source': f'/opt/airflow/etl/blocks.csv',
+                   'target': 'blocks/'},
+        dag=dag
+    )
 
-#         command = "export_all -s 0 -e  200 -p %s.".format(ENV_DATADIR_EXTERNAL, ENV_NODE_ETHEREUM),
+    t1 >> t2 >> t3 >> upload_to_s3
