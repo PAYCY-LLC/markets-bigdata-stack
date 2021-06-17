ad828cd866b3c84411a0d99f169e98740f956ab5
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-06-17 14:22:07 +0600

    Divide "ethereum_export_dag" tasks into chunks

--- docker-airflow-etl/dags/ethereum_export_dag.py
+++ docker-airflow-etl/dags/ethereum_export_dag.py
@@ -1,11 +1,10 @@
 import os
-import shutil
 from datetime import datetime, timedelta
 
 import boto3
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.docker_operator import DockerOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 ENV_S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
@@ -18,7 +17,6 @@ ENV_DATADIR_EXTERNAL = os.getenv("DATADIR_EXTERNAL")
 
 default_args = {
     'owner': 'airflow',
-    'description': 'Use of the DockerOperator',
     'depend_on_past': False,
     'start_date': datetime(2018, 1, 3),
     'email_on_failure': False,
@@ -27,7 +25,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *", catchup=False) as dag:
+with DAG('ethereum_export_dag', default_args=default_args, catchup=False) as dag:
     s3_client = boto3.client(
         's3',
         region_name=ENV_S3_REGION,
@@ -36,17 +34,49 @@ with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *",
         aws_secret_access_key=ENV_S3_SECRET_KEY
     )
 
+    task_start = DummyOperator(
+        task_id='start',
+        dag=dag
+    )
+
+    task_end = DummyOperator(
+        task_id='end',
+        dag=dag)
+
+
+    def export_all(start, end, batch):
+        return DockerOperator(
+            task_id=f'export_from_{start}_to_{end}',
+            image='blockchainetl/ethereum-etl:latest',
+            api_version='auto',
+            auto_remove=True,
+            volumes=[f'{ENV_DATADIR_EXTERNAL}/etl:/ethereum-etl/output'],
+            command=f'export_all -s {start} -e {end} -b {batch} -p {ENV_NODE_ETHEREUM}',
+            docker_url="unix://var/run/docker.sock",
+            network_mode="bridge"
+        )
+
+
+    def upload_to_s3(start, end):
+        return PythonOperator(
+            task_id=f's3_upload_from_{start}_to_{end}',
+            python_callable=upload_files,
+            op_kwargs={
+                'path': '/opt/airflow/etl',
+                'start': start,
+                'end': end
+            },
+            dag=dag
+        )
+
 
     # ----------------------------- Upload dir  -------------------------------------------
-    def upload_files(path, start, end):
+    def upload_files(path):
         for root, dirs, files in os.walk(path):
             for file in files:
                 file_path = os.path.join(root, file)
 
                 with open(file_path, 'rb') as data:
-                    dir_parent = os.path.join(root, '../..')
-                    dir_current = os.path.dirname(root)
-
                     s3_client.put_object(
                         Bucket=ENV_S3_BUCKET_NAME,
                         Key=file_path[len(path) + 1:],
@@ -54,39 +84,14 @@ with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *",
                     )
                     os.remove(file_path)
 
-                    if start == dir_current[-len(start):]:
-                        shutil.rmtree(dir_parent, ignore_errors=True)
-                    if end == dir_current[-len(end):]:
-                        shutil.rmtree(dir_parent, ignore_errors=True)
 
+    chunk = 0
+    batch = 10_000
 
-    # ----------------------------- Tasks -------------------------------------------
+    for i in range(0, 1_000_000, batch):
+        chunk += batch
 
-    start_task = BashOperator(
-        task_id='print_current_date',
-        bash_command='date'
-    )
+        task_export = export_all(i, chunk, batch)
+        task_upload = upload_to_s3(i, chunk)
 
-    etl_export = DockerOperator(
-        task_id='docker_command',
-        image='blockchainetl/ethereum-etl:latest',
-        api_version='auto',
-        auto_remove=True,
-        volumes=[f'{ENV_DATADIR_EXTERNAL}/etl:/ethereum-etl/output'],
-        command=f'export_all -s 1000000 -e 1001000 -b 1000 -p {ENV_NODE_ETHEREUM}',
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge"
-    )
-
-    upload_to_s3 = PythonOperator(
-        task_id='upload_to_s3',
-        python_callable=upload_files,
-        op_kwargs={
-            'path': '/opt/airflow/etl',
-            'start': '1000000',
-            'end': '1001000'
-        },
-        dag=dag
-    )
-
-    start_task >> etl_export >> upload_to_s3
+        task_start >> task_export >> task_upload >> task_end
